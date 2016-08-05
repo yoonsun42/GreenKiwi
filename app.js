89ad73248ac4f0f7abbef51ca2abafa7a633cfb6
Jaejeon <jaejeon9@gmail.com>
2016-08-05 20:38:11 +0900

    윤선이 poll.js 작성 시작

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var poll = require('./modules/poll.js');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+poll();
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ modules/poll.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jaejeon on 2016-08-05.
+ */
+
+module.exports = function(){
+    
+};
